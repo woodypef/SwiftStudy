6fd0b5ab148022cd2aa4049aea8d70a026f8edfa
woodypef <ssumt1mes@naver.com>
2022-12-06 13:16:06 +0900

    12-06 13:15 mac os commit

---
+++ MyFirstApp/MyFirstApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MyFirstApp
+//
+//  Created by woodypef on 2022/12/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyFirstApp/MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by woodypef on 2022/12/06.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
